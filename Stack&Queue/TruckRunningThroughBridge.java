ef0640ab8c6b11f3465db7a817678f22d54b5014
rochabye <reng731@gmail.com>
2019-02-28 16:32:28 +0900

    Declaration queue

---
+++ Stack&Queue/TruckRunningThroughBridge.java
@@ -0,0 +1,9 @@
+import java.util.Queue;
+import java.util.LinkedList;
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+       Queue< Integer > q = new LinkedList< Integer >();
+        return answer;
+    }
+}
