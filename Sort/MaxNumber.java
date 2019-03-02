2d308850cf2192d1379b5594586f3cd2e36afcc1
rochabye <reng731@gmail.com>
2019-03-02 23:16:42 +0900

    initial

---
+++ Sort/MaxNumber.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        int[] 
+        while( true ) {
+            
+        }
+        int num_length = (int)(Math.log10(a)+1);
+        return answer;
+    }
+}
