0b9f5d6c74dbe65bf5e06981aab8f7164bb6bd9f
rochabye <reng731@gmail.com>
2019-03-03 14:38:02 +0900

    push truck in queue

--- Stack&Queue/TruckRunningThroughBridge.java
+++ Stack&Queue/TruckRunningThroughBridge.java
@@ -3,7 +3,12 @@ import java.util.LinkedList;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
-       Queue< Integer > q = new LinkedList< Integer >();
+        Queue< Integer > q = new LinkedList< Integer >();
+        int cnt = 0;
+        while( cnt < truck_weights.length ) {
+            q.push( truck_weights[ cnt ] );
+            ++cnt;
+        }
         return answer;
     }
 }
