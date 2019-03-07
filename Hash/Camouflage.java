edc7ce1206e8f66d7f12dfa7a88ca8d7cb4232b1
rochabye <reng731@gmail.com>
2019-03-07 15:52:49 +0900

    hash/Camouflage : setting hash map from data

---
+++ Hash/Camouflage.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        HashMap< String, Integer > cloth_map = new HashMap< String, Integer >();
+        
+        for( int i = 0; i < clothes.length; ++i ) {
+            cloth_map.put( clothes[i][1], cloth_map.getOrDefault( clothes[i][1], 0 ) + 1 );
+        }
+
+        Iterator< String > it = cloth_map.keySet().iterator();
+        if ( cloth_map.keySet().size() > 1 ) {
+            answer = cloth_map.get( it.next() );
+            while( it.hasNext() ) {
+                answer = answer * cloth_map.get( it.next() ); 
+            }
+        }
+        it = cloth_map.keySet().iterator();
+        while( it.hasNext() ) {
+            answer = answer + cloth_map.get( it.next() );
+        }
+        return answer;
+    }
+}
