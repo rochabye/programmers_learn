a169719c0de2d577e716940c05b72d98513107d9
rochabye <reng731@gmail.com>
2019-03-10 22:49:16 +0900

    calc multi

--- Hash/Camouflage.java
+++ Hash/Camouflage.java
@@ -18,10 +18,25 @@ class Solution {
                 answer = answer * cloth_map.get( it.next() ); 
             }
         }
+        
         it = cloth_map.keySet().iterator();
+        ArrayList< Integer > type = new ArrayList< Integer >();
         while( it.hasNext() ) {
-            answer = answer + cloth_map.get( it.next() );
+            String key = it.next();
+            answer = answer + cloth_map.get( key ); // sum each one 
+            type.add( cloth_map.get(key ) );
         }
+        ArrayList< Integer > type_multi = new ArrayList< Integer >();
+        for( int i = 0 ; i < type.size(); ++i ) {
+            for( int j = i + 1; j < type.size(); ++j ) {
+                type_multi.add( type.get( i ) * type.get( j ) );
+            }
+        }
+        int all_multi = 1;
+        for( int i = 0; i < type.size(); ++ i ) {
+            all_multi *= type.get(i);
+        }
+        
         return answer;
     }
 }
