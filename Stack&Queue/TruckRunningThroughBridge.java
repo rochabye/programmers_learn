437e6b2cb23f73f3c158a0ef25db5dfde3e47d9e
rochabye <reng731@gmail.com>
2019-03-13 19:51:26 +0900

    Truck.. : add queue class init

--- Stack&Queue/TruckRunningThroughBridge.java
+++ Stack&Queue/TruckRunningThroughBridge.java
@@ -1,5 +1,55 @@
 import java.util.Queue;
 import java.util.LinkedList;
+
+class Queue<T> {
+	
+	class Node<T> {
+		private T data;
+		private Node<T> next;
+		
+		public Node( T data ) {
+			this.data = data;
+		}
+	}
+	
+	private Node<T> first;
+	private Node<T> last;
+	
+	public void enQueue( T item ) {
+		Node<T> t = new Node<T>( item );
+		if ( last != null ) {
+			last.next = t;
+		}
+		last = t;
+		if ( first == null ) {
+			first = last;
+		}
+	}
+	
+	public T remove() {
+		if ( first == null ) {
+			return null;
+		}
+		T data = first.data;
+		first = first.next;
+		if ( first == null ) {
+			last = null;
+		}
+		return data;
+	}
+	
+	public T peek() {
+		if ( first == null ) {
+			return null;
+		}
+		return first.data;
+	}
+	
+	public boolean isEmpty() {
+		return first == null;
+	}
+}
+
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
