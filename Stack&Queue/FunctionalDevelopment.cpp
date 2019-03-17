8791d7e43b35e35a887a2ff55f43a7335b13e76e
rochabye <reng731@gmail.com>
2019-03-17 18:59:54 +0900

    FunctionalDevelopment : fix bug. solved

--- Stack&Queue/FunctionalDevelopment.cpp
+++ Stack&Queue/FunctionalDevelopment.cpp
@@ -3,23 +3,23 @@
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
-    vector<int> answer;
+	vector<int> answer;
 	int pop_count = 0;
 	while (!progresses.empty()) {
 		for (int i = 0; i < progresses.size(); ++i) {
 			progresses[i] += speeds[i];
 		}
 		if (progresses.front() >= 100) {
-			for (int i = 0; i < progresses.size(); ++i) {
-				if (progresses[i] >= 100) {
-					pop_count++;
-				}
-			}
+            int i = 0;
+            while( progresses[i] >= 100 ) {
+                pop_count++;
+                i++;
+            }
 			progresses.erase(progresses.begin(), progresses.begin() + pop_count);
+			speeds.erase(speeds.begin(), speeds.begin() + pop_count);
 			answer.push_back(pop_count);
-			pop_count = 0; 
+			pop_count = 0;
 		}
 	}
-
-    return answer;
+	return answer;
 }
