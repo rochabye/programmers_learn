2b8bfa1e829acfe9411676730ca38f2a8884ceb7
rochabye <reng731@gmail.com>
2019-03-24 19:42:43 +0900

    todo : mini heap pop impl

--- Heap/MoreSpice.cpp
+++ Heap/MoreSpice.cpp
@@ -3,12 +3,11 @@
 
 using namespace std;
 
-
 void PushHeap(vector< int >& heap, int new_value) {
 	heap.push_back(new_value);
 	int index = heap.size() - 1;
 
-	while (index > 0 && heap[(index - 1) / 2] < heap[index]) {
+	while (index > 0 && heap[(index - 1) / 2] > heap[index]) {
 		swap(heap[index], heap[(index - 1) / 2]);
 		index = (index - 1) / 2;
 	}
@@ -23,13 +22,14 @@ void PopHeap(vector< int >& heap) {
 		int left = here * 2 + 1, right = here * 2 + 2;
 		if (left >= heap.size()) break;
 		int next = here;
-		if (heap[next] < heap[left]) next = left;
-		if (right < heap.size() && heap[next] < heap[right]) next = right;
+		if ( heap[ left ] < heap[next] ) next = left;
+		if ( right < heap.size() && heap[right] < heap[next]) next = right;
 		if (next == here) break;
 		swap(heap[here], heap[next]);
 		here = next;
 	}
 }
+
 int MakeScoville(int a, int b) {
 	int result = 0;
 	if (a < b) {
